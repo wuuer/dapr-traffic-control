5dfb2df9698a1eb01ce52270e133b9f141fbf844
JunFeng Wu <ad7790h@qq.com>
2023-04-17 09:27:55 +0800

    Update IVehicleRegistrationService.cs

--- src/FineCollectionService/Proxies/IVehicleRegistrationService.cs
+++ src/FineCollectionService/Proxies/IVehicleRegistrationService.cs
@@ -3,15 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using FineCollectionService.Models;
 using System.Threading.Tasks;
-using ServiceInvocation.DaprClient.Attributes;
 
 namespace FineCollectionService.Proxies
 {
-    [DaprClientProxy("vehicleregistrationservice")]
     public interface IVehicleRegistrationService
     {
-        [GetMethod("vehicleinfo/{licenseNumber}")]
-        public Task<VehicleInfo> GetVehicleInfo([PathString]string licenseNumber);
+        public Task<VehicleInfo> GetVehicleInfo(string licenseNumber);
 
 
     }
